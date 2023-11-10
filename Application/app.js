f7a7e1b8a0f57ec28f5b668e24b4da4ddfa3a383
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-10 20:44:07 +0800

    Update app.js

--- Application/app.js
+++ Application/app.js
@@ -6,28 +6,9 @@ const port = 3000;
 const app = express();
 
 app.use(express.static('/Application'));
-app.set('views', './views');
+
 app.use(express.json());
-app.set('/main', 'main.hbs');
-app.set('/home', 'home.hbs');
 
-app.get('/', (req, res) => {
-    res.redirect('/main');
-});
-
-app.get('/main', (req, res) => {
-    res.render('index', {
-        title: "DLSU Lab Reservation"
-    });
- });
-
- app.get('/main/index', (req, res) => {
-    const acctChoice = JSON.parse(acctChoice);
-    if (acctChoice.acct === "login"){
-        res.render('Login', {
-        });
-    }
- });
 
 
 
