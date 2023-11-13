998d499c5c730d994e32368340ca3ce8eb11a5e1
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-14 02:21:03 +0800

    Update app.js

--- Application/app.js
+++ Application/app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = requires('./routes.js');
 const server = requires('./public/javascript/database/server.js');
 
 const port = 3000;
 
 const app = express();
+
+const url = 'mongodb+srv://grantvillamor04:zH1nSlCxTr93sYSS@cluster0.rz5d7np.mongodb.net/'
+
+async function connect() {
+    try{
+        await mongoose.connect(url);
+    }catch(error){
+        console.log(error);
+    }
+}
+
+connect();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({
@@ -18,7 +31,7 @@ app.use(bodyParser.json());
 
 app.use('/', routes);
 
-server.connect();
+
 
 
 app.listen(port, () => {
