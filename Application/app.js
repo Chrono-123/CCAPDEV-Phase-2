de99d0b120d6e7c1f0d3024674754858ba5db9d6
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-14 02:34:04 +0800

    Update app.js

--- Application/app.js
+++ Application/app.js
@@ -8,17 +8,7 @@ const port = 3000;
 
 const app = express();
 
-const url = 'mongodb+srv://grantvillamor04:zH1nSlCxTr93sYSS@cluster0.rz5d7np.mongodb.net/'
-
-async function connect() {
-    try{
-        await mongoose.connect(url);
-    }catch(error){
-        console.log(error);
-    }
-}
-
-connect();
+server.connect();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({
