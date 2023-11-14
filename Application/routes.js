49637fc42588714438914aeba863b96e3492569f
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-14 11:11:11 +0800

    Update routes.js

--- Application/routes.js
+++ Application/routes.js
@@ -7,13 +7,13 @@ app.get('/', function(req, res) => {
   res.send('/html/main');
 });
 
-app.get('/html/register', function(req, res) => {
+app.get('/html/register', function(req, res) => { //html -> node js -> mongodb
   const data = {fName, lName, birth, user, password} = req.body;
   db.collection("laboratory").insertOne('data', function(req, res){
     db.close();
 });
 
-app.post('/html/profile', function(req, res) => {
+app.post('/html/profile', function(req, res) => { //mongodb -> node js -> html
   const data = {fName, lName, birth, user, password};
   db.collection("laboratory").findOne({'data'}, function(err, result)(
     db.close()
