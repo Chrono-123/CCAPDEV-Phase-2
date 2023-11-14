336b0101e21cf7680ec870a1201f1a66c962158d
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-14 15:03:04 +0800

    Update routes.js

--- Application/routes.js
+++ Application/routes.js
@@ -1,25 +1,22 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('/public/javascript/database/server.js');
-const app = express();
-
-app.get('/', function(req, res) => {
-  res.send('/html/main');
+routes.route("/profile/:id").get(function(req,res){
+	Restaurant.findById(req.param.id).then((laboratory) => {
+		res.json(restaurant);
+		res.end();
+	});
 });
 
-app.get('/html/register', function(req, res) => { //html -> node js -> mongodb
-  const data = {fName, lName, birth, user, password} = req.body;
-  db.collection("laboratory").insertOne('data', function(req, res){
-    db.close();
+routes.route("/labTechReservation").get(function(req,res){
+	Restaurant.find().then((laboratory) => {
+		res.json(laboratory);
+		res.end();
+	});
 });
 
-app.post('/html/profile', function(req, res) => { //mongodb -> node js -> html
-  const data = {fName, lName, birth, user, password};
-  db.collection("laboratory").findOne({'data'}, function(err, result)(
-    db.close()
-    );
-  res.send{json.stringify({
-    
-  });
-  };
-};
+routes.route("/profile").post(function(req,res){
+	const restaurant = new Restaurant({
+		name: req.body.name,
+		address: req.body.address,
+	});
+	restaurant.save();
+	res.json({message: "Restaurant saved"});
+});
