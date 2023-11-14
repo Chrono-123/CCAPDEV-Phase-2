36e4843af36fbcfb6771b072f40c934dd0f25e8a
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-14 22:37:56 +0800

    Update app.js

--- CCAPDEV-Phase-2-main/CCAPDEV-Phase-2-main/Application/app.js
+++ CCAPDEV-Phase-2-main/CCAPDEV-Phase-2-main/Application/app.js
@@ -1,26 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const routes = requires('./routes.js');
-const server = requires('./public/javascript/database/server.js');
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import "dotenv/config";
+import routes from './routes.js';
 
 const port = 3000;
 
 const app = express();
 
-server.connect();
-
-app.use(express.static('public'));
-app.use(express.urlencoded({
-    extended: true
-});
-app.use(bodyParser.urlencoded({
-    extended:true
-});
+app.use(cors());
 app.use(bodyParser.json());
+app.use(
+	bodyParser.urlencoded({
+			extended: true,
+	})
+);
 
-app.use('/', routes);
-
+app.use(routes);
 
 
 
