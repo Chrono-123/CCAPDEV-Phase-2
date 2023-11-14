c5d68702d2a3f55c5c9179c322f3199c541cbe55
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-14 10:19:30 +0800

    Update routes.js

--- Application/routes.js
+++ Application/routes.js
@@ -3,15 +3,26 @@ const bodyParser = require('body-parser');
 const db = require('/public/javascript/database/server.js');
 const app = express();
 
-app.get('/', function(req, res){
+app.get('/', function(req, res) => {
   res.send('/html/main');
 });
 
-app.get('/html/register', function(req, res){
+app.get('/html/register', function(req, res) => {
   const data = {fName, lName, birth, user, password} = req.body;
   db.collection("laboratory").insertOne('data', function(req, res){
     db.close();
 });
 
-app.post('/html/profile', function(req, res){
-  
+app.post('/html/profile', function(req, res) => {
+  const data = {fName, lName, birth, user, password};
+  db.collection("laboratory").findOne({}, function(err, result)(
+    db.close()
+    );
+  res.send{
+    fName,
+    lName,
+    birth,
+    user,
+    password
+  };
+}
