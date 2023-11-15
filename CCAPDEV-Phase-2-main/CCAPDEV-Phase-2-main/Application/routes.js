3b320451c7aa146e95cc37c48a980ddd335dcc1c
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-15 18:36:34 +0800

    Update routes.js

--- CCAPDEV-Phase-2-main/CCAPDEV-Phase-2-main/Application/routes.js
+++ CCAPDEV-Phase-2-main/CCAPDEV-Phase-2-main/Application/routes.js
@@ -6,7 +6,12 @@ app.get('/login', function(req, res) => {
  res.
 });
 
-app.post('/profile', function(req, res) => { 
+app.post('/profile', function(req, res) => {
+ const fName = await db.findOne();
+ const lName = await db.findOne();
+ const uName = await db.findOne();
+ const password = await db.findOne();
+  
  res.send{json.stringify({
   })
 });
