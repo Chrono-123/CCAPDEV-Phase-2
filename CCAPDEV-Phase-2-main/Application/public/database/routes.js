f79ea11c6930446ea5ce169f53fa5285f93e99a7
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-16 12:22:20 +0800

    Update routes.js

--- CCAPDEV-Phase-2-main/Application/public/database/routes.js
+++ CCAPDEV-Phase-2-main/Application/public/database/routes.js
@@ -5,20 +5,6 @@ import { getDb } from './conn.js';
 const app = express();
 
 const db = getDb();
-    // const data = {fname, lname, birth, user, password} = req.body;
-    db.collection("test").insertOne({
-        firstName: "Sean",
-        lastName: "Lim",
-        dateOfBirth: "11/10/01",
-        userName: "Jesus",
-        password: "Iamjesus"
-    }).then(val => {
-        console.log("Insert successful: ");
-        console.log(val);
-    }).catch(error => {
-        console.log("Insert op error: " + error);
-    });
-
 
 app.get('/', function(req, res){
   res.send('/html/main');
