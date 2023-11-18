42ac2296ccb3654917d8638c1150936d62d25df1
Chrono-123 <145533807+Chrono-123@users.noreply.github.com>
2023-11-18 12:20:32 +0800

    Update mainController.js

---
+++ CCAPDEV-Phase-2-main/Application/controllers/mainController.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', function(req, res) {
+   res.redirect('/main');
+});
+
+app.get('/main', function(req, res) {
+   res.render('main');
+});
+
+module.exports = app;
