090a74679eae718dc324f5c3f87ae10a83fd72ca
SeanHigginsLim <132881963+SeanHigginsLim@users.noreply.github.com>
2023-12-08 19:39:12 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ dotenv.config();
 
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/lab');
+mongoose.connect('mongodb://0.0.0.0:27017/lab');
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
